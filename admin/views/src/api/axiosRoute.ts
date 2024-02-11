cac1d85b6ed41a2196b38779869cbf4a1081cae5
Ace G <aceguevarra48@gmail.com>
2024-02-11 18:21:21 +0800

    Latest

--- admin/views/src/api/axiosRoute.ts
+++ admin/views/src/api/axiosRoute.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-export const axiosRoute = axios.create({
-  baseURL: "http://localhost:5030",
-});
-
 export const axiosPrivateRoute = axios.create({
   baseURL:
     "http://ec2-54-206-111-44.ap-southeast-2.compute.amazonaws.com:5050/",
