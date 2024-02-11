beedc4bf667191955d6dd23bef9979288f14aaa7
Ace G <aceguevarra48@gmail.com>
2024-02-11 18:12:35 +0800

    Changed Client to Server URL

--- admin/views/src/api/axiosRoute.ts
+++ admin/views/src/api/axiosRoute.ts
@@ -5,6 +5,7 @@ export const axiosRoute = axios.create({
 });
 
 export const axiosPrivateRoute = axios.create({
-  baseURL: "http://localhost:5050",
+  baseURL:
+    "http://ec2-54-206-111-44.ap-southeast-2.compute.amazonaws.com:5050/",
   withCredentials: true,
 });
