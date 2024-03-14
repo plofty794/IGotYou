0c29e5e955c60c934195b19c6daa85cae5299781
Ace G <aceguevarra48@gmail.com>
2024-03-14 11:31:16 +0800

    Updated code

--- admin/views/src/api/axiosRoute.ts
+++ admin/views/src/api/axiosRoute.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosPrivateRoute = axios.create({
-  baseURL: "https://igot-you.online/api",
+  baseURL: "http://localhost:5040",
   withCredentials: true,
 });
+
+// https://igot-you.online/api
